fa4a0397fae342b8f5685c83630489b668f96de5
booyaChael <chaeeul2022@gmail.com>
2023-04-24 20:57:37 +0900

    MealItem에 form 추가

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -4,14 +4,14 @@ import MealItemForm from "./MealItemForm";
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
   return (
-    <li>
-      <div className={classes.meal}>
+    <li className={classes.meal}>
+      <div>
         <h3>{props.name}</h3>
-        <div>{props.description}</div>
-        <div>{price}</div>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
