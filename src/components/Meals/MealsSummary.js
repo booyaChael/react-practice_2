00c2bb7db6e4bf5505fe4a300950376d34cbde1c
booyaChael <chaeeul2022@gmail.com>
2023-04-24 20:03:49 +0900

    Meals 마크업

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,13 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>Choose your favorite meals</p>
+      <p>All our meals are cooked in fresh ingredients</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
